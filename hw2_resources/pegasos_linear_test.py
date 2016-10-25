218597879ca55018c30991c16901cbd39f9d8ac7
Jose I Velarde M <joseivelarde@dhcp-18-189-101-237.dyn.MIT.EDU>
2016-10-24 23:51:03 -0400

    I <3 Jeff

--- hw2_resources/pegasos_linear_test.py
+++ hw2_resources/pegasos_linear_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from plotBoundary import *
 import pylab as pl
+
 # import your LR training code
 
 
@@ -10,10 +11,12 @@ X = train[:,0:2]
 Y = train[:,2:3]
 
 # Carry out training.
+def kernel(x1,x2):
+    return np.dot(x1, x2)
+
 def pegasos_alg(X,Y,lambd,max_iter):
 	t = 0
 	w = np.zeros(X.shape[1]+1)
-	print w.shape
 	iterations = 0
 	while iterations < max_iter:
 		for i in xrange(len(X)):
@@ -24,28 +27,106 @@ def pegasos_alg(X,Y,lambd,max_iter):
 			theta = w[1:]
 			theta_0 = w[0]
 			if y_i*(np.dot(theta,x_i) + theta_0) < 1:
-
+				# print 'theta',theta
 				theta = (1-learning_rate*lambd)*theta + learning_rate*y_i*x_i
 				theta_0 = theta_0 + learning_rate*y_i
-				w = np.concatenate((theta_0,theta),axis=0)
+				w = np.insert(theta,0,theta_0)
+				# w = np.concatenate((theta_0,theta),axis=0)
 			else:
-				theta = (1-learning_rate*lambd)*theta + learning_rate*y_i*x_i
-				w = np.concatenate((theta_0,theta),axis=0)
+				theta = (1-learning_rate*lambd)*theta
+				# print theta_0, theta
+				# w = np.concatenate(([theta_0],theta),axis=0)
+				w = np.insert(theta,0,theta_0)
+		# print w
 		iterations += 1
-
+	print w
+	print 1.0/np.linalg.norm(w)
 	return w
 
-w = pegasos_alg(X,Y,10,10**4)
-print w
+def pegasos_alg_2(X,Y,lambd,max_iter):
+	t = 0
+	ones = np.ones([X.shape[0],1])
+	new_X = np.concatenate((ones,X),axis=1)
+	w = np.zeros(new_X.shape[1])
+	iterations = 0
+	while iterations < max_iter:
+		for i in xrange(len(X)):
+			t += 1
+			learning_rate = 1.0/(t*lambd)
+			y_i = Y[i]
+			x_i = new_X[i]
+			theta = w.copy()
+			theta_0 = w[0]
+			# print theta_0
+			if y_i*(np.dot(theta,x_i)) < 1:
+				# print 'theta',theta
+				theta = (1-learning_rate*lambd)*theta + learning_rate*y_i*x_i
+				theta_0 = theta_0 + learning_rate*y_i
+				theta[0] = theta_0
+				w = theta
+			else:
+				theta = (1-learning_rate*lambd)*theta 
+				# print theta_0, theta
+				theta[0] = theta_0
+				w = theta
+		# print w
+		iterations += 1
+	print w
+	print 1.0/np.linalg.norm(w)
+	return w
 
+def kernelized_pegasos_alg(X,Y,lambd,K,max_iter):
+	t = 0
+	# w = np.zeros(X.shape[1])
+	alphas = np.zeros(X.shape[0])
+	iterations = 0
+	while iterations < max_iter:
+		for i in xrange(len(X)):
+			t += 1
+			learning_rate = 1.0/(t*lambd)
+			y_i = Y[i]
+			alpha_i = alphas[i]
+
+			summ = 0
+			for j in range(len(alphas)):
+				summ += alphas[j]*K[j][i]
+
+			if y_i*summ < 1:
+				alphas[i] = (1-learning_rate*lambd)*alpha_i + learning_rate*y_i 
+
+			else:
+				alphas[i] = (1-learning_rate*lambd)*alpha_i 
+
+		iterations += 1
+
+	return alphas
+
+def build_K_matrix(X,kernel):
+	n = X.shape[0]
+	K = np.zeros([n,n])
+	for i in xrange(n):
+		for j in xrange(n):
+			K[i][j] = kernel(X[i],X[j])
+	return K
 # Define the predict_linearSVM(x) function, which uses global trained parameters, w
 ### TODO: define predict_linearSVM(x) ###
+
+
+
+
+def predict_linearSVM_kernelized(x):
+	summ = 0
+	for i in alphas:
+		summ += alphas[i]*kernel(X[i],x)
+	return np.sign(summ)
+
 def predict_linearSVM(x):
 	theta_0 = w[0]
 	theta = w[1:]
-	return np.sign(np.dot(theta,x)+theta_0)
+	return np.dot(theta,x)+theta_0
 
-# plot training results
-plotDecisionBoundary(X, Y, predict_linearSVM, [-1,0,1], title = 'Linear SVM')
-pl.show()
+# # plot training results
+
+# plotDecisionBoundary(X, Y, predict_linearSVM, [-1,0,1], title = 'Linear SVM')
+# pl.show()
 
